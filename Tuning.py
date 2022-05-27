75594fa12d68e21b9b60665213a0a1f84fd56ac7
Julie Butler Hartley <73757257+juliebutlerhartley@users.noreply.github.com>
2022-05-27 10:09:49 -0400

    Create Tuning.py

---
+++ Tuning.py
@@ -0,0 +1,8 @@
+import numpy as np
+from Regression import *
+
+def tune_ridge_regression(alpha_range = [0,1e-6,1e-4,1e-2,1,10,100]):
+    
+def tune_polynomial_kernel_ridge_regresssion(gamma_range=np.arange(-5.0,5.5,0.5), 
+                                             c0_range=np.arange(-5.0,5.5,0.5),p_range=[1,2,3,4],
+                                             alpha_range=[0,1e-6,1e-4,1e-2,1,10,100]):
